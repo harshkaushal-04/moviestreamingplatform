36ec4d5b38e4f61e6acbb49fdcdc954ad2ee401c
Harsh kaushal <147242440+harshkaushal-04@users.noreply.github.com>
2025-05-21 02:52:41 +0530

    Updated Header.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,12 +3,22 @@ const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
 const MovieCard = ({ movie }) => {
   return (
-    <img
-      src={IMAGE_BASE_URL + movie.poster_path}
-      className="w-[110px] md:w-[200px] rounded-lg
-    hover:border-[3px] border-gray-400 cursor-pointer
-    hover:scale-110 transition-all duration-150 ease-in"
-    />
+    <div className="relative group">
+      <img
+        src={IMAGE_BASE_URL + movie.poster_path}
+        className="w-[110px] md:w-[200px] rounded-lg
+        group-hover:border-[3px] border-gray-400 cursor-pointer
+        group-hover:scale-110 transition-all duration-300 ease-in-out"
+        alt={movie.title}
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-all duration-300 rounded-lg flex items-center justify-center">
+        <div className="opacity-0 group-hover:opacity-100 transform group-hover:translate-y-0 translate-y-4 transition-all duration-300">
+          <button className="bg-white text-black px-6 py-2 rounded-full font-semibold hover:bg-opacity-80">
+            Play
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
